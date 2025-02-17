69d2710ae3ad47500f7e738ff6a96900261f8fe9
jjvdc22003 <jjvdc22003@gmail.com>
2025-02-17 10:57:26 -0600

    SCRUM-68

--- resources/views/components/trabajemos-juntos/formulario-contactanos.blade.php
+++ resources/views/components/trabajemos-juntos/formulario-contactanos.blade.php
@@ -1,25 +1,47 @@
 <!-- Contenedor del formulario -->
 <div class="formularioContactanosContainer">
+    <!-- Titulo del formulario -->
     <div class="formularioContactanosTitle">¡Contáctanos y exploremos juntos las posibilidades!</div>
     <!-- Formulario "Contáctanos" -->
     <form action="" method="post" class="formularioContactanos">
         <!-- Espacio para Nombre y Apellidos -->
         <div class="nombreApellidosContainer">
             <!-- Campo de texto "Nombre" -->
-            <input type="text" class="campo-nombre" name="nombre" id="nombre" placeholder="Nombre" required>
+            <div class="inputContainer">
+                <input type="text" class="campo-nombre" name="nombre" id="nombre" required>
+                <label for="nombre">Nombre</label>
+            </div>
             <!-- Campo de texto "Apellidos" -->
-            <input type="text" class="campo-apellidos" name="apellidos" id="apellidos" placeholder="Apellidos" required>
+            <div class="inputContainer">
+                <input type="text" class="campo-apellidos" name="apellidos" id="apellidos" required>
+                <label for="apellidos">Apellidos</label>
+            </div>
         </div>
         <!-- Campo para el Correo electrónico -->
-        <input type="email" name="email" id="email" placeholder="Correo electrónico" required>
+        <div class="inputContainer">
+            <input type="email" name="email" id="email" required>
+            <label for="email">Correo electrónico</label>
+        </div>
         <!-- Campo para el número de telefono -->
-        <input type="tel" name="telefono" id="telefono" pattern="[\+]?[\d\s\-]{7,15}" placeholder="Teléfono celular" required>
+        <div class="inputContainer">
+            <input type="tel" name="telefono" id="telefono" pattern="[\+]?[\d\s\-]{7,15}" required>
+            <label for="telefono">Teléfono celular</label>
+        </div>
         <!-- Campo para el país -->
-        <input type="text" name="pais" id="pais" placeholder="País" required>
+        <div class="inputContainer">
+            <input type="text" name="pais" id="pais" required>
+            <label for="pais">País</label>
+        </div>
         <!-- Campo para la empresa -->
-        <input type="text" name="empresa" id="empresa" placeholder="Empresa" required>
+        <div class="inputContainer">
+            <input type="text" name="empresa" id="empresa" required>
+            <label for="apellidos">Empresa</label>
+        </div>
         <!-- Campo para háblanos de tu proyecto -->
-        <textarea name="hablanos-de-tu-proyecto" id="hablanos-de-tu-proyecto" placeholder="Háblanos de tu proyecto" required></textarea>
+        <div class="textAreaContainer">
+            <textarea name="hablanos-de-tu-proyecto" id="hablanos-de-tu-proyecto" required></textarea>
+            <label for="hablanos-de-tu-proyecto">Háblanos de tu proyecto</label>
+        </div>
         <!-- Contenedor para los términos y condiciones -->
         <div class="terminosYBotonContainer">
             <!-- Términos y condiciones -->
